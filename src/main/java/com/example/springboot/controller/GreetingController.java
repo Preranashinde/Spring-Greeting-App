b13aad250a921e0143afccc894c013e5f13e7060
Preranashinde <preranapshinde3009@gmail.com>
2020-06-14 11:56:44 -0300

    ADDED:Extended greeting controoler to use service layer to get greeting message hello world

--- src/main/java/com/example/springboot/controller/GreetingController.java
+++ src/main/java/com/example/springboot/controller/GreetingController.java
@@ -1,16 +1,27 @@
 package com.example.springboot.controller;
 
+
+import com.example.springboot.service.GreetingAppService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class GreetingController {
 
+    @Autowired
+    private GreetingAppService greetingAppService;
+
     @GetMapping(value = "/greeting", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity greetingMessage() {
-        return new ResponseEntity("Welcome to Greeting App", HttpStatus.OK);
+
+        public ResponseEntity sayWelcome() {
+            return new ResponseEntity("Welcome to Greeting App", HttpStatus.OK);
+        }
+        @GetMapping(value = "/greeting/message", produces = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity greetingMessage() {
+            return new ResponseEntity(greetingAppService.greetingMessage(), HttpStatus.OK);
+        }
+
     }
-}
---
+++ src/main/java/com/example/springboot/service/GreetingAppService.java
@@ -0,0 +1,11 @@
+package com.example.springboot.service;
+import org.springframework.stereotype.Service;
+
+    @Service
+    public class GreetingAppService {
+
+        public String greetingMessage() {
+            return "Hello World\n";
+        }
+    }
+
